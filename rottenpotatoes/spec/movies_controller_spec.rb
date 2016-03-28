32ea129b6d23667da108b1fb3d6344520083d165
arielkaluzhny <akaluzhny@wesleyan.edu>
2016-03-28 21:31:02 +0000

    created RSpec test skeleton in spec folder

---
+++ rottenpotatoes/spec/movies_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe MoviesController do
+    describe 'find movies with same director' do
+        it 'should find a list of movies with the same director' do
+            #write test here
+        end
+        it 'should redirect to the homepage if there is no director information' do
+            #write test here
+        end
+    end
+end
